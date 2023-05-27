5b7f997079f073912de00d2e936e82f52fc84844
Felicia Ng <feliciangwanjun@gmail.com>
2023-05-27 11:11:57 +0800

    Added Containerised Backend Nodejs app

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM node:16-alpine
+
+WORKDIR /my-app
+
+COPY package*.json ./
+
+RUN npm install 
+
+COPY . .
+
+EXPOSE 8080
+
+CMD ["node", "index.js"]
---
+++ index.js
@@ -0,0 +1,26 @@
+// index.js (Express app)
+
+const express = require('express');
+
+const app = express();
+
+// Endpoint 1: /
+app.get('/', (req, res) => {
+  res.statusCode = 200;
+  const msg = 'Hello from Node!';
+  res.send(msg);
+});
+
+// Endpoint 2: /test
+app.get('/test', (req, res) => {
+  res.statusCode = 200;
+  const msg = 'Hello from /test Node!';
+  res.send(msg);
+});
+
+// Start the server
+const server = app.listen(8080, () => {
+  console.log('Server started on http://localhost:8080');
+});
+
+module.exports = server; // Export server instance for testing
