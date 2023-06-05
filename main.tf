f014d3026c386449c5779725e5d74f069b8c20e8
Wan Jun Ng <feliciangwanjun@gmail.com>
2023-06-05 14:51:42 +0800

    TF VPC and ECS Cluster successfully provisioned

--- main.tf
+++ main.tf
@@ -25,3 +25,64 @@ module "vpc" {
   }
 }
 
+## ECS Module
+
+module "ecs" {
+  source = "terraform-aws-modules/ecs/aws"
+
+  cluster_name = "group3-ecs-tf"
+
+  fargate_capacity_providers = {
+    FARGATE = {
+      default_capacity_provider_strategy = {
+        weight = 100
+      }
+    }
+  }
+
+  services = {
+    group3-ecs = { #td name
+      cpu    = 512
+      memory = 1024
+
+      # Container definition(s)
+      container_definitions = {
+
+        ecs-sample = { #container name
+          essential = true 
+          image     = "public.ecr.aws/docker/library/httpd:latest"
+          port_mappings = [
+            {
+              name          = "ecs-sample"
+              containerPort = 8080
+              protocol      = "tcp"
+            }
+          ]
+          readonly_root_filesystem = false
+
+        }
+      }
+      assign_public_ip = true
+      deployment_minimum_healthy_percent = 100
+      subnet_ids = ["subnet-0c367963f6cb16292", "subnet-0db7c7648426ccf6f"]
+      security_group_rules = {
+        ingress_all = {
+          type                     = "ingress"
+          from_port                = 0
+          to_port                  = 0
+          protocol                 = "-1"
+          description              = "Allow all"
+          cidr_blocks = ["0.0.0.0/0"]
+        }
+        egress_all = {
+          type        = "egress"
+          from_port   = 0
+          to_port     = 0
+          protocol    = "-1"
+          cidr_blocks = ["0.0.0.0/0"]
+        }
+      }
+    }
+  }
+
+}
