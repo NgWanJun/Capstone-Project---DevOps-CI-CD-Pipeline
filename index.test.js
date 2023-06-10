5df458f012829b51d1636993c8e99144286e80da
Wan Jun Ng <feliciangwanjun@gmail.com>
2023-06-10 11:56:25 +0800

    Testing 1

--- index.test.js
+++ index.test.js
@@ -9,7 +9,7 @@ describe('Express App', () => {
   it('should respond with "Hello from Farmers Market! Demo" when hitting the root endpoint', async () => {
     const response = await request(server).get('/');
     expect(response.status).toBe(200);
-    expect(response.text).toBe('Hello from Farmers Market!');
+    expect(response.text).toBe('Hello from Farmers Market! Demo');
   });
 
 /*   it('should respond with "Hello from /test Node!" when hitting the /test endpoint', async () => {
